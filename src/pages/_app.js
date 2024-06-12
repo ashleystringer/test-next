f37e204f36048d1f22ffe86291f86dbea93f808b
ashleystringer <ashstrin@umail.iu.edu>
2024-06-12 12:03:13 -0400

    added NavBar to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,10 @@
+import NavBar from "../components/NavBar";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <NavBar />
+      <Component {...pageProps} />
+    </div>  
+    );
 }
