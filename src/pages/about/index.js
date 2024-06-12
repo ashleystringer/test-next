49b617d8b04c44f84a5e6040e407bfc8b52d2aff
ashleystringer <ashstrin@umail.iu.edu>
2024-06-12 11:49:03 -0400

    added about directory

---
+++ src/pages/about/index.js
@@ -0,0 +1,11 @@
+export default function About() {
+    return (
+      <>
+        <main>
+          <div>
+            This is the About page.
+          </div>
+        </main>
+      </>
+    );
+  }
