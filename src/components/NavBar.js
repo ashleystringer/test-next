2fc3b8b4729f5eea3777598bcf316e5938be18c6
ashleystringer <ashstrin@umail.iu.edu>
2024-06-12 11:58:18 -0400

    added NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            About the extension
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
